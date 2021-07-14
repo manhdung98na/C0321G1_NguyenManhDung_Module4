e4ca0127e98941786903c41d98bf6d39512585bf
manhdung98na <md0949470893@gmail.com@github.com>
2021-07-14 16:20:49 +0700

    chỉnh sửa lại cho đúng cấu trúc của Spring

--- 01-spring-mvc-overview/exercises/01-simple-dictionary/src/main/java/com/example/controller/DictionaryController.java
+++ 01-spring-mvc-overview/exercises/01-simple-dictionary/src/main/java/com/example/controller/DictionaryController.java
@@ -1,21 +1,14 @@
 package com.example.controller;
 
+import com.example.service.DictionaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
 public class DictionaryController {
@@ -24,31 +17,14 @@ public class DictionaryController {
         return "/index";
     }
 
+    @Autowired
+    DictionaryService dictionaryService;
+
     @PostMapping("/translate")
-    public String translate(@RequestParam("txtSearch") String txtSearch, Model model) throws IOException {
-        System.out.println("input " + txtSearch);
-        String result = "Không tìm thấy kết quả phù hợp";
-
-        URL url = new URL("https://vdict.com/" + txtSearch + ",1,0,0.html");
-        Scanner scanner = new Scanner(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
-        scanner.useDelimiter("\\Z");
-        String content = scanner.next();
-        // close scanner
-        scanner.close();
-
-        // regex
-        Pattern p = Pattern.compile("class=\"list1\"><li><b>(.*?)</b>");
-        Matcher m = p.matcher(content);
-        //tạo queue để lưu trữ, tránh trùng lặp tin tức
-        while (m.find()) {
-            result = m.group(1);
-            break;
-        }
-        System.out.println("result " + result);
+    public String translate(@RequestParam("txtSearch") String txtSearch, Model model) {
+        String result = dictionaryService.translate(txtSearch);
         model.addAttribute("txtSearch", txtSearch);
         model.addAttribute("result", result);
         return "/result";
     }
-
-
 }
--- 01-spring-mvc-overview/exercises/02-convert-currency/src/main/java/com/codegym/controller/CurrencyController.java
+++ 01-spring-mvc-overview/exercises/02-convert-currency/src/main/java/com/codegym/controller/CurrencyController.java
@@ -1,5 +1,7 @@
 package com.codegym.controller;
 
+import com.codegym.service.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,12 @@ public class CurrencyController {
         return "/index";
     }
 
+    @Autowired
+    CurrencyService currencyService;
+
     @PostMapping("/convert")
     public String convert(@RequestParam("rate") float rate, @RequestParam("usd") float usd, Model model){
-        System.out.println(rate);
-        System.out.println(usd);
-        float vnd = rate * usd;
+        float vnd = currencyService.convert(usd,rate);
         model.addAttribute("usd", usd);
         model.addAttribute("vnd", vnd);
         return "/result";
