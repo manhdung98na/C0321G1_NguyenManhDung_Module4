939e3170232deb9a63f933b8b5f33c6d63526f69
manhdung98na <md0949470893@gmail.com@github.com>
2021-07-25 17:44:46 +0700

    Bài 8: Validation - Bài thực hành 2

---
+++ 08-validation/practices/02-customer-validation/src/main/java/com/codegym/Application.java
@@ -0,0 +1,22 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("validation-message");
+        return messageSource;
+    }
+}
