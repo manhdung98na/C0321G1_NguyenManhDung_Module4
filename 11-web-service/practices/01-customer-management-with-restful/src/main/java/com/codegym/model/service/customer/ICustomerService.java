33d5e9b72e17453532ce9e4708bdc5327c642f6c
manhdung98na <md0949470893@gmail.com@github.com>
2021-07-29 18:27:20 +0700

    Bài 11: Web Service - Bài thực hành 1

---
+++ 11-web-service/practices/01-customer-management-with-restful/src/main/java/com/codegym/model/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.codegym.model.service.customer;
+
+import com.codegym.model.entity.Customer;
+import com.codegym.model.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
